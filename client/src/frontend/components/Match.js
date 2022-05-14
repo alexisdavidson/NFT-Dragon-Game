a5e2dfcf142cc3cae6d305b1ddfc44b43514c216
Alexis Davidson <alexisdavidson00@gmail.com>
2022-05-14 15:13:59 +0200

    Update fight frontend

--- client/src/frontend/components/Match.js
+++ client/src/frontend/components/Match.js
@@ -63,79 +63,105 @@ const Match = () => {
 
     return (
         <div className="flex justify-center">
-            <h2>Fight!</h2>
-            {/* <p>Winner of match {location.state.matchId} is: {match.winner}</p> */}
-            {/* <p>Battle log: {match.battle_log}</p> */}
-            <table className="table table-bordered table-striped table-dark">
-                <thead>
-                    <tr>
-                        <th scope="col">Dragon</th>
-                        <th scope="col">Attack</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {JSON.parse(match.battle_log).map((val) => {
-                        return (
-                            <tr>
-                                <th scope="row">{val.dragon}</th>
-                                <td>
-                                    {val.isCriticalStrike ? 
-                                        <b className="text-danger"> 
-                                            {val.attackValue}
-                                        </b>
+            {items.length > 0 ?
+                <div className="px-5 container">
+                    <h2>Fight!</h2>
+                    {/* <p>Winner of match {location.state.matchId} is: {match.winner}</p> */}
+                    {/* <p>Battle log: {match.battle_log}</p> */}
+                    <Row className="g-4 d-flex justify-content-center">
+                        <Col>
+                            <Card bg="dark">
+                                <Card.Img variant="top" src={items[0].image_url} />
+                                <Card.Body color="secondary">
+                                <Card.Title>{items[0].name}</Card.Title>
+                                <Card.Text>
+                                    {items[0].owner.address}
+                                    <br/>
+                                    <br/>
+                                    Attack: {items[0].traits.filter(e => e.trait_type == "Attack")[0].value}
+                                    <br/>
+                                    Defense: {items[0].traits.filter(e => e.trait_type == "Defense")[0].value}
+                                    <br/>
+                                    Luck: {items[0].traits.filter(e => e.trait_type == "Luck")[0].value}
+                                </Card.Text>
+                                </Card.Body>
+                                <Card.Footer>
+                                <div className='d-grid'>
+                                    {match.winner == 1 ?
+                                    <h2 className="text-success">Win</h2>
                                     :
-                                        <span> 
-                                            {val.attackValue}
-                                        </span>
+                                    <h2 className="text-danger">Lose</h2>
                                     }
-                                </td>
-                            </tr>
-                        );
-                    })}
-                </tbody>
-            </table>
+                                </div>
+                                </Card.Footer>
+                            </Card>
+                        </Col>
+                        
+                        <Col>
+                            <table className="table table-bordered table-striped table-dark">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">Dragon</th>
+                                        <th scope="col">Attack</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {JSON.parse(match.battle_log).map((val) => {
+                                        return (
+                                            <tr>
+                                                <th scope="row">{val.dragon}</th>
+                                                <td>
+                                                    {val.isCriticalStrike ? 
+                                                        <b className="text-danger"> 
+                                                            {val.attackValue}
+                                                        </b>
+                                                    :
+                                                        <span> 
+                                                            {val.attackValue}
+                                                        </span>
+                                                    }
+                                                </td>
+                                            </tr>
+                                        );
+                                    })}
+                                </tbody>
+                            </table>
+                        </Col>
 
-            <div className="flex justify-center">
-                {items.length > 0 ?
-                    <div className="px-5">
-                        <Row xs={1} md={2} lg={4} className="g-4 py-5 d-flex justify-content-center">
-                            {items.map((item, idx) => (
-                                <Col key={idx} className="overflow-hidden">
-                                    <Card bg="dark">
-                                        <Card.Img variant="top" src={item.image_url} />
-                                        <Card.Body color="secondary">
-                                        <Card.Title>{item.name}</Card.Title>
-                                        <Card.Text>
-                                            {item.owner.address}
-                                            <br/>
-                                            <br/>
-                                            Attack: {item.traits.filter(e => e.trait_type == "Attack")[0].value}
-                                            <br/>
-                                            Defense: {item.traits.filter(e => e.trait_type == "Defense")[0].value}
-                                            <br/>
-                                            Luck: {item.traits.filter(e => e.trait_type == "Luck")[0].value}
-                                        </Card.Text>
-                                        </Card.Body>
-                                        <Card.Footer>
-                                        <div className='d-grid'>
-                                            {idx + 1 == match.winner ?
-                                            <h2 className="text-success">Win</h2>
-                                            :
-                                            <h2 className="text-danger">Lose</h2>
-                                            }
-                                        </div>
-                                        </Card.Footer>
-                                    </Card>
-                                </Col>
-                            ))}
-                        </Row>
-                    </div>
-                : (
-                    <main style={{ padding: "1rem 0" }}>
-                        <h2>Loading...</h2>
-                    </main>
-                )}
-            </div>
+                        <Col>
+                            <Card bg="dark">
+                                <Card.Img variant="top" src={items[1].image_url} />
+                                <Card.Body color="secondary">
+                                <Card.Title>{items[1].name}</Card.Title>
+                                <Card.Text>
+                                    {items[1].owner.address}
+                                    <br/>
+                                    <br/>
+                                    Attack: {items[1].traits.filter(e => e.trait_type == "Attack")[0].value}
+                                    <br/>
+                                    Defense: {items[1].traits.filter(e => e.trait_type == "Defense")[0].value}
+                                    <br/>
+                                    Luck: {items[1].traits.filter(e => e.trait_type == "Luck")[0].value}
+                                </Card.Text>
+                                </Card.Body>
+                                <Card.Footer>
+                                <div className='d-grid'>
+                                    {match.winner == 2 ?
+                                    <h2 className="text-success">Win</h2>
+                                    :
+                                    <h2 className="text-danger">Lose</h2>
+                                    }
+                                </div>
+                                </Card.Footer>
+                            </Card>
+                        </Col>
+                    </Row>
+                </div>
+            : (
+                <main style={{ padding: "1rem 0" }}>
+                    <h2>Loading...</h2>
+                </main>
+            )}
         </div>
     );
 }
